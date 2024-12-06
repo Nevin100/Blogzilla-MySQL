6b355569a2de5183fe907d74a27cfff9e7edfd98
Nevin-Bali100 <opopopopop100000@gmail.com>
2024-12-06 09:59:07 +0530

    new commit

--- FrontEnd/src/pages/Login.jsx
+++ FrontEnd/src/pages/Login.jsx
@@ -1,18 +1,55 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+
 const Login = () => {
+  const [Inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+  const [err, setError] = useState(null); //for error management
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  const navigate = useNavigate();
+
+  //sending data
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:8800/api/auth/login", Inputs);
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
   return (
     <div className="auth">
       <h1>Login</h1>
       <form>
-        <input className="" required placeholder="UserName" type="text" />
-        <input className="" required placeholder="Password" type="password" />
+        <input
+          name="username"
+          required
+          placeholder="UserName"
+          type="text"
+          onChange={handleChange}
+        />
+        <input
+          required
+          name="password"
+          placeholder="Password"
+          type="password"
+          onChange={handleChange}
+        />
         <span className="">
           No account?? {"  "}
-          <Link to="/register">Register</Link>
+          <Link to="/register">
+            <h5>Register</h5>
+          </Link>
         </span>
-        <button>Login</button>
-        <p>This is an error</p>
+        <button onClick={handleSubmit}>Login</button>
+        {err && <p style={{ color: "red" }}>{err}</p>}
       </form>
     </div>
   );
